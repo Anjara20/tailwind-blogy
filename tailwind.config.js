115e2ea482277784da2e3ae8e1a5d227c6764877
Anjaramamy Liantsoa <andrianilanaliantsoa6533@gmail.com>
2021-07-19 14:53:27 +0300

    version faharoa import fonts et colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "cw-grey-050":"F0F4B",
+        "cw-grey-100":"#D9E2EC",
+        "cw-grey-700":"#334E68",
+        "cw-grey-800":"#234B53",
+        "cw-grey-900":"#102A43",
+        "cw-blue-300":"#62B0E8",
+        "cw-blue-400":"#4098D7",
+        "cw-blue-600":"#186FAF",
+
+      },
+      fontFamily:{
+        'roboto':['Roboto', 'sans-serif'],
+        'work-sans':['Work Sans', 'sans-serif'],
+      }
+    },
   },
   variants: {
     extend: {},
